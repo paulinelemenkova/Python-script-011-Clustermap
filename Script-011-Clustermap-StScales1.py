691e4016f8e2dbb19d6e61649f798ed7393088f0
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-19 20:39:41 +0800

    Add files via upload

--- Script-011-Clustermap-StScales1.py
+++ Script-011-Clustermap-StScales1.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
-#
+import os
 import numpy as np
 import pandas as pd
-import seaborn as sb
 from matplotlib import pyplot as plt
-import os
-#
-os.chdir('/Users/pauline/Documents/Python')
+import seaborn as sb
+
 sb.set(style="white")
-df = pd.read_csv("Tab-Bathy.csv")
 sb.set(color_codes=True)
-#
+sb.set_context('paper')
+
+os.chdir('/Users/pauline/Documents/Python')
+df = pd.read_csv("Tab-Bathy.csv")
+
+# define variables and plotting
 g = sb.clustermap(df, standard_scale=1)
 rotation = 45
 for i, ax in enumerate(g.fig.axes):   # getting all axes of the fig object
-     ax.set_xticklabels(ax.get_xticklabels(), rotation = rotation)
-#
+     ax.set_xticklabels(ax.get_xticklabels(),
+                        rotation = rotation
+                        )
 g.fig.suptitle('Mariana Trench: Clustermap of the bathymetric observations. \nDataset: 25 cross-section profiles, 518 observations in each. \nMethod: standard scales = 1') 
-plt.subplots_adjust(bottom=0.20,top=0.90, right=0.90, left=0.10)
+plt.subplots_adjust(bottom=0.20,top=0.90,
+                    right=0.90, left=0.10
+                    )
+
+# visualizing and saving
+plt.savefig('plot_ClustSc.png', dpi=300)
 plt.show()
